dc6e20a8bb962ce923505f466001ca37a9dc3241
Alexey Shkarin <alexey.shkarin@mpl.mpg.de>
2022-01-23 22:53:38 +0100

    Trace plotter expanded

--- pylablib/gui/widgets/plotters/trace_plotter.py
+++ pylablib/gui/widgets/plotters/trace_plotter.py
@@ -18,6 +18,7 @@ import pyqtgraph
 
 from ....core.gui import QtCore, Signal
 import numpy as np
+import collections
 
 
 class TracePlotterCtl(QWidgetContainer):
@@ -44,6 +45,7 @@ class TracePlotterCtl(QWidgetContainer):
         self.add_group_box("channels_box",caption="Channels").add_child("channels",self.channels_table,gui_values_path="channels")
         self.channels_table.setMinimumSize(QtCore.QSize(20,20))
         self.channels_table.setup(add_indicator=False)
+        self.channels_table.contained_value_changed.connect(self.channels_updated)
         self.setup_channels()
         self.plot_params_table=ParamTable(self)
         self.add_group_box("plotting_box",caption="Plotting").add_child("plotting",self.plot_params_table,gui_values_path="plotting")
@@ -55,6 +57,7 @@ class TracePlotterCtl(QWidgetContainer):
         self.add_padding()
         self.main_layout.setStretch(2,1)
 
+    channels_updated=Signal(object,object)
     def setup_channels(self):
         """
         Update channels from the associated plotter.
@@ -73,6 +76,10 @@ class TracePlotterCtl(QWidgetContainer):
     def get_enabled_channels(self):
         """Get a list of enabled channels"""
         return [idx for idx in self.plotter.channel_indices if self.channels_table.v[idx+"_enabled"]]
+
+
+
+
 
 
 mpl_colors=['#1f77b4','#ff7f0e','#2ca02c','#d62728','#9467bd','#8c564b','#e377c2','#bcbd22','#17becf']
@@ -94,7 +101,7 @@ class TracePlotter(QLayoutManagedWidget):
         super().__init__(parent)
         self.name=None
         self.ctl=None
-    def setup(self, name=None, add_end_marker=False, update_only_on_visible=True):  # pylint: disable=arguments-differ, arguments-renamed
+    def setup(self, name=None, add_end_marker=False, update_only_on_visible=True, full_data=False):  # pylint: disable=arguments-differ, arguments-renamed
         """
         Setup the image view.
 
@@ -102,6 +109,8 @@ class TracePlotter(QLayoutManagedWidget):
             name (str): widget name
             add_end_marker (bool): if ``True``, point markers are added at the position of the last point (makes easier to track plotting progress).
             update_only_on_visible (bool): if ``True``, only update plot if the widget is visible.
+            full_data (bool): if ``True``, whenever data is updated from the source, all channels are requested and stored;
+                this makes it a bit more CPU and memory-heavy, but allows reconfiguring the plot while paused
         """
         self.name=name
         super().setup(layout="vbox",no_margins=True)
@@ -118,9 +127,12 @@ class TracePlotter(QLayoutManagedWidget):
         self.data_src=None
         self.add_end_marker=add_end_marker
         self.update_only_on_visible=update_only_on_visible
+        self.full_data=full_data
         self.displayed=[]
         self.vlines=[]
         self.vmarks=[]
+        self._drag_lines={}
+        self._plotted_data=None
     
     def _attach_controller(self, ctl):
         """
@@ -128,7 +140,15 @@ class TracePlotter(QLayoutManagedWidget):
 
         Called automatically in :meth:`TracePlotterCtl.setup`, doesn't need to be called explicitly.
         """
+        if self.ctl is not None and self.ctl is not ctl:
+            raise RuntimeError("a different controller is already attached")
         self.ctl=ctl
+        self.ctl.channels_updated.connect(controller.exsafe(lambda: self._draw_plot()))  # pylint: disable=unnecessary-lambda
+        @controller.exsafe
+        def update_lines(name, _):
+            if name=="xaxis":
+                self._update_drag_lines()
+        self.ctl.channels_updated.connect(update_lines)
 
 
     def setup_channels(self, channels, channel_indices=None):
@@ -168,6 +188,88 @@ class TracePlotter(QLayoutManagedWidget):
         self.on_reset.emit()
 
 
+    TDragLine=collections.namedtuple("TDragLine",["channels","positions","line"])
+    def add_vertical_line(self, name, channels="all"):
+        """
+        Add a draggable vertical line to the plot.
+
+        `channels` can specify x-axis channels for which this line is visible (``"all"`` means all channels).
+        """
+        if name in self._drag_lines:
+            raise ValueError("line {} already exists".format(name))
+        line=pyqtgraph.InfiniteLine(angle=90,movable=True)
+        line.setZValue(20)
+        self.plot_widget.getPlotItem().getViewBox().addItem(line,ignoreBounds=True)
+        self._drag_lines[name]=self.TDragLine(channels,{},line)
+        line.sigPositionChanged.connect(self._make_line_updater(name),QtCore.Qt.DirectConnection)
+        self._update_drag_lines()
+    def _make_line_updater(self, name):
+        @controller.exsafe
+        def update():
+            xaxis=self.ctl.v["channels/xaxis"] if self.ctl else 0
+            self._drag_lines[name].positions[xaxis]=self._drag_lines[name].line.getPos()[0]
+        return update
+    def _update_drag_lines(self, par=None):
+        if par is None:
+            xaxis=self.ctl.v["channels/xaxis"] if self.ctl else 0
+        else:
+            xaxis=par["channels/xaxis"]
+        for dln in self._drag_lines.values():
+            if dln.channels=="all" or self.channel_indices[xaxis] in dln.channels:
+                visible=True
+                if xaxis in dln.positions:
+                    dln.line.setPos(dln.positions[xaxis])
+            else:
+                visible=False
+            dln.line.setPen("g" if visible else None)
+            dln.line.setHoverPen("y" if visible else None)
+            dln.line.setMovable(visible)
+    def is_line_visible(self, name):
+        """Check if the line with the given name is visible"""
+        return self._drag_lines[name].line.movable
+    def get_line_position(self, name, channel=None, only_visible=True):
+        """
+        Get the position of a line with the given name for a given channel.
+
+        If `channel` is ``None``, use the current x-axis channel.
+        If ``only_visible==True`` and the line is currently invisible, return ``None``.
+        """
+        if only_visible and not self.is_line_visible(name):
+            return None
+        if channel is None:
+            channel=self.ctl.v["channels/xaxis"]
+        else:
+            channel=self.channel_indices.index(channel)
+        return self._drag_lines[name].positions.get(channel,None)
+    def set_line_position(self, name, position, channel=None, only_visible=True):
+        """
+        Set the position of a line with the given name for a given channel.
+
+        If `channel` is ``None``, use the current x-axis channel.
+        If ``only_visible==True`` and the line is currently invisible, do nothing.
+        """
+        if only_visible and not self.is_line_visible(name):
+            return
+        xaxis=self.ctl.v["channels/xaxis"]
+        if channel is None:
+            channel=xaxis
+        else:
+            channel=self.channel_indices.index(channel)
+        if channel==xaxis:
+            self._drag_lines[name].line.setPos(position)
+        else:
+            self._drag_lines[name].positions[channel]=position
+    def center_line(self, name, only_visible=True):
+        """
+        Center line with the given name within the plot range.
+
+        If ``only_visible==True`` and the line is currently invisible, do nothing.
+        """
+        if only_visible and not self.is_line_visible(name):
+            return
+        rng=self.plot_widget.getAxis("bottom").range
+        self._drag_lines[name].line.setPos((rng[0]+rng[1])/2)
+
 
     def _update_plot_lines(self):
         """
@@ -202,7 +304,7 @@ class TracePlotter(QLayoutManagedWidget):
     
     def get_required_channels(self):
         """Get list of channels required for plotting: all enabled channels plus 'X axis' and  'Order by' channels """
-        if self.ctl is None:
+        if self.ctl is None or self.full_data:
             return self.channel_indices
         xaxis=self.channel_indices[self.ctl.channels_table.v["xaxis"]]
         order_by=self.channel_indices[self.ctl.channels_table.v["order_by"]]
@@ -261,7 +363,48 @@ class TracePlotter(QLayoutManagedWidget):
             return self.data_src.reset()
 
 
-
+    def _draw_plot(self, data=None, par=None, idx_column=None):
+        """
+        Draw the plot using the given data, parameters dictionary, and index column.
+        
+        If data is not supplied, use the last plotted data (if none is available, do nothing).
+        """
+        if data is None:
+            data=self._plotted_data
+        if data is None:
+            return
+        if self.full_data:
+            self._plotted_data=data
+        if par is None:
+            par=self.ctl.get_all_values() if self.ctl else {"channels/xaxis":0, "channels/order_by":0, "plotting/update_plot":True}
+        xaxis=self.channel_indices[par["channels/xaxis"]]
+        order_by=self.channel_indices[par["channels/order_by"]]
+        data_channels=[xaxis,order_by]+self.get_enabled_channels()
+        norm_data=[]
+        for idx in data_channels:
+            col=np.asarray(data[idx])
+            if "factor" in self.channels[idx]:
+                col=col*self.channels[idx]["factor"]
+            norm_data.append(col)
+        if norm_data and len(norm_data[0]):
+            last_pts=[col[-1] for col in norm_data]
+        else:
+            last_pts=None
+        if order_by!=idx_column:
+            norm_data=np.column_stack(norm_data)
+            norm_data=trace_utils.sort_by(norm_data,x_column=1,stable=True)
+            norm_data=[norm_data[:,c] for c in range(norm_data.shape[1])]
+        autorange=self.plot_widget.plotItem.getViewBox().autoRangeEnabled()
+        self.plot_widget.plotItem.disableAutoRange()
+        for vl,col in zip(self.vlines,norm_data[2:]):
+            vl.setData(norm_data[0],col)
+        if last_pts:
+            for vm,pt in zip(self.vmarks,last_pts[2:]):
+                if vm is not None:
+                    vm.setData([last_pts[0]],[pt])
+        if any(autorange):
+            self.plot_widget.plotItem.enableAutoRange(x=autorange[0],y=autorange[1])
+        self.plot_widget.setLabel("bottom",self.channels[xaxis]["legend_name"])
     def update_plot(self, data=None, idx_column=None):
         """
         Update plot data.
@@ -276,34 +419,8 @@ class TracePlotter(QLayoutManagedWidget):
         if par["plotting/update_plot"] and (self.isVisible() or not self.update_only_on_visible):
             if data is None:
                 data=self.get_data_from_source()
-            xaxis=self.channel_indices[par["channels/xaxis"]]
-            order_by=self.channel_indices[par["channels/order_by"]]
-            data_channels=[xaxis,order_by]+self.get_enabled_channels()
-            norm_data=[]
-            for idx in data_channels:
-                col=np.asarray(data[idx])
-                if "factor" in self.channels[idx]:
-                    col=col*self.channels[idx]["factor"]
-                norm_data.append(col)
-            if norm_data and len(norm_data[0]):
-                last_pts=[col[-1] for col in norm_data]
-            else:
-                last_pts=None
-            if order_by!=idx_column:
-                norm_data=np.column_stack(norm_data)
-                norm_data=trace_utils.sort_by(norm_data,x_column=1,stable=True)
-                norm_data=[norm_data[:,c] for c in range(norm_data.shape[1])]
-            autorange=self.plot_widget.plotItem.getViewBox().autoRangeEnabled()
-            self.plot_widget.plotItem.disableAutoRange()
-            for vl,col in zip(self.vlines,norm_data[2:]):
-                vl.setData(norm_data[0],col)
-            if last_pts:
-                for vm,pt in zip(self.vmarks,last_pts[2:]):
-                    if vm is not None:
-                        vm.setData([last_pts[0]],[pt])
-            if any(autorange):
-                self.plot_widget.plotItem.enableAutoRange(x=autorange[0],y=autorange[1])
-            self.plot_widget.setLabel("bottom",self.channels[xaxis]["legend_name"])
+            self._draw_plot(data=data,par=par,idx_column=idx_column)
+            
 
 
 
@@ -318,11 +435,11 @@ class TracePlotterCombined(QWidgetContainer):
     The plotter can be accessed as ``.plt`` attribute, and the controller as ``.ctl`` attribute.
     The ``"sidebar"`` sublayout can be used to add additional elements if necessary.
     """
-    def setup(self, add_end_marker=False, update_only_on_visible=True, name=None):  # pylint: disable=arguments-differ, arguments-renamed
+    def setup(self, add_end_marker=False, update_only_on_visible=True, full_data=False, name=None):  # pylint: disable=arguments-differ, arguments-renamed
         super().setup(layout="hbox",no_margins=True,name=name)
         self.plt=TracePlotter(self)
         self.add_to_layout(self.plt)
-        self.plt.setup(name="plt",add_end_marker=add_end_marker,update_only_on_visible=update_only_on_visible)
+        self.plt.setup(name="plt",add_end_marker=add_end_marker,update_only_on_visible=update_only_on_visible,full_data=full_data)
         with self.using_new_sublayout("sidebar","vbox"):
             self.ctl=TracePlotterCtl(self)
             self.add_child("ctl",self.ctl)
