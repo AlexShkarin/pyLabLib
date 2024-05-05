2af3e064e839713257bd313b326ceed9a45454fb
Alexey Shkarin <alexey.shkarin@mpl.mpg.de>
2024-05-05 10:35:54 +0200

    Build and tox update

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
     ],
     project_urls={
     'Documentation': 'https://pylablib.readthedocs.io',
